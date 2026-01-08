83c42beaa1260e4d7ffbd298e14901d7f862991d
Beatific <1telpenkov1@mail.ru>
2026-01-08 15:14:27 +0300

    main

--- src/test/java/ru/netology/OrderingCardDeliveryTest.java
+++ src/test/java/ru/netology/OrderingCardDeliveryTest.java
@@ -1,54 +1,39 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Selenide.*;
 
 class RegistrationTest {
 
+    @BeforeAll
+    static void setUp() {
+        Configuration.headless = true;
+    }
+
     @Test
     void shouldRegisterByCardDelivery() {
 
-        // открываем страницу
         Selenide.open("http://localhost:9999");
-
-        // дата: сегодня + 3 дня
-        String date = LocalDate.now()
-                .plusDays(3)
-                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
-        // город
+        String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=city] input").setValue("Москва");
-
-        // очищаем поле даты (clear() тут НЕ работает)
-        $("[data-test-id=date] input")
-                .press("\u0001") // Ctrl + A
-                .press("\u0008"); // Backspace
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(date);
-
-        // имя
         $("[data-test-id=name] input").setValue("Иванов Иван");
-
-        // телефон
         $("[data-test-id=phone] input").setValue("+79001234567");
-
-        // чекбокс
         $("[data-test-id=agreement]").click();
-
-        // кнопка
         $("button.button").click();
-
-        // состояние загрузки (не более 15 сек)
-        $("[data-test-id=notification]")
-                .shouldBe(Condition.visible, Duration.ofSeconds(15));
-
-        // проверка текста
-        $("[data-test-id=notification] .notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована"));
+        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        String notificationText = $("[data-test-id=notification] .notification__content").getText();
+        System.out.println("Текст уведомления: " + notificationText);
+        $("[data-test-id=notification] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + date));
     }
 }
